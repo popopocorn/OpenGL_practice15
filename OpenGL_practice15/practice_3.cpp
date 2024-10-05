d51ddecf9b34a421706b1cc84f69039895707440
popopocorn <popcorn375@naver.com>
2024-10-05 21:32:01 +0900

    삼각형 이동 및 꼭짓점 접촉시 병합

--- OpenGL_practice15/practice_3.cpp
+++ OpenGL_practice15/practice_3.cpp
@@ -7,7 +7,7 @@ GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 GLvoid Mouse(int button, int state, int x, int y);
-
+GLvoid change_xy(int x, int y);
 
 std::random_device rd;
 std::mt19937 g(rd());
@@ -18,6 +18,7 @@ GLclampf base_g = 1.0f;
 GLclampf base_b = 1.0f;
 
 void generate_rect();
+
 
 typedef struct random_rect {
 	bool is_creat = false;
@@ -54,6 +55,7 @@ void main(int argc, char** argv) {
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);
 	glutMouseFunc(Mouse);
+	glutMotionFunc(change_xy);
 	glutMainLoop();
 
 }
@@ -111,10 +113,71 @@ void generate_rect() {
 	}
 }
 
+bool is_drag = false;
+int selected_rect = -1;
+int merge_target = -1;
+bool is_merged = false;
+GLclampf offset_x, offset_y;
+
+
 GLvoid Mouse(int button, int state, int x, int y) {
-	GLclampf mouse_x = 0.0f;
-	GLclampf mouse_y = 0.0f;
-	mouse_x = (float)(x - (float)800 / 2.0) * (float)(1.0 / (float)(800 / 2.0));
-	mouse_y = -(float)(y - (float)600 / 2.0) * (float)(1.0 / (float)(600 / 2.0));
-	
+	GLclampf mouse_x = (float)(x - (float)800 / 2.0) * (float)(1.0 / (float)(800 / 2.0));
+	GLclampf mouse_y = -(float)(y - (float)600 / 2.0) * (float)(1.0 / (float)(600 / 2.0));
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		for (int cur = 0; cur < 10; ++cur) {
+			if (rectangle_list[cur].x1 < mouse_x && mouse_x < rectangle_list[cur].x2 && rectangle_list[cur].y2 < mouse_y && mouse_y < rectangle_list[cur].y1) {
+				is_drag = true;
+				selected_rect = cur;
+				offset_x = mouse_x - rectangle_list[cur].x1;
+				offset_y = mouse_y - rectangle_list[cur].y1;
+				break;
+
+			}
+		}
+	}
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+		for (int i = 0; i < 10; ++i) {
+			if ((rectangle_list[i].x1 < rectangle_list[selected_rect].x1 && rectangle_list[selected_rect].x1 < rectangle_list[i].x2 && rectangle_list[i].y2 < rectangle_list[selected_rect].y1 && rectangle_list[selected_rect].y1 < rectangle_list[i].y1)
+				|| (rectangle_list[i].x1 < rectangle_list[selected_rect].x1 && rectangle_list[selected_rect].x1 < rectangle_list[i].x2 && rectangle_list[i].y2 < rectangle_list[selected_rect].y2 && rectangle_list[selected_rect].y2 < rectangle_list[i].y1)
+				|| (rectangle_list[i].x1 < rectangle_list[selected_rect].x2 && rectangle_list[selected_rect].x2 < rectangle_list[i].x2 && rectangle_list[i].y2 < rectangle_list[selected_rect].y1 && rectangle_list[selected_rect].y1 < rectangle_list[i].y1)
+				|| (rectangle_list[i].x1 < rectangle_list[selected_rect].x2 && rectangle_list[selected_rect].x2 < rectangle_list[i].x2 && rectangle_list[i].y2 < rectangle_list[selected_rect].y2 && rectangle_list[selected_rect].y2 < rectangle_list[i].y1)) {
+				merge_target = i;
+				is_merged = true;
+				break;
+
+			}
+		}
+		if(is_merged){
+			
+			rectangle_list[merge_target].x1 = std::min(rectangle_list[merge_target].x1, rectangle_list[selected_rect].x1);
+			rectangle_list[merge_target].y1 = std::max(rectangle_list[merge_target].y1, rectangle_list[selected_rect].y1);
+			rectangle_list[merge_target].x2 = std::max(rectangle_list[merge_target].x2, rectangle_list[selected_rect].x2);
+			rectangle_list[merge_target].y2 = std::min(rectangle_list[merge_target].y2, rectangle_list[selected_rect].y2);
+			rectangle_list[selected_rect].is_creat = false;
+			
+		}
+
+		merge_target = -1;
+		is_merged = false;
+		is_drag = false;
+		selected_rect = -1;
+	}
+	glutPostRedisplay();
+
+}
+
+GLvoid change_xy(int x, int y) {
+	GLclampf width{ rectangle_list[selected_rect].x2 - rectangle_list[selected_rect].x1 };
+	GLclampf height{ rectangle_list[selected_rect].y1 - rectangle_list[selected_rect].y2 };
+	if (is_drag && selected_rect != -1) {
+		GLclampf mouse_x = (float)(x - (float)800 / 2.0) * (float)(1.0 / (float)(800 / 2.0));
+		GLclampf mouse_y = -(float)(y - (float)600 / 2.0) * (float)(1.0 / (float)(600 / 2.0));
+		rectangle_list[selected_rect].x1 = mouse_x - offset_x;
+		rectangle_list[selected_rect].y1 = mouse_y - offset_y;
+		rectangle_list[selected_rect].x2 = rectangle_list[selected_rect].x1 + width;
+		rectangle_list[selected_rect].y2 = rectangle_list[selected_rect].y1 - height;
+
+
+	}
+	glutPostRedisplay();
 }
