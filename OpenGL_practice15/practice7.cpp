ddcf7fcae7a019bf92dbb158d6cc8d7d14bd9175
popopocorn <popcorn375@naver.com>
2024-10-11 00:54:00 +0900

    Update practice7.cpp

--- OpenGL_practice15/practice7.cpp
+++ OpenGL_practice15/practice7.cpp
@@ -2,22 +2,17 @@
 #include<gl/glew.h>
 #include<gl/freeglut.h>
 #include<gl/freeglut_ext.h>
-#include"file_read.h"
 
-void make_vertexShaders();
-void make_fragmentShaders();
-GLuint make_shaderProgram();
+
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 
-GLint width, height;
-GLuint shaderProgramID;
-GLuint vertexShader;
-GLuint fragmentShader;
 
-
+GLclampf base_r = 1.0f;
+GLclampf base_g = 1.0f;
+GLclampf base_b = 1.0f;
 
 
 void main(int argc, char** argv) {
@@ -36,15 +31,6 @@ void main(int argc, char** argv) {
 	{
 		std::cout << "GLEW initialized\n";
 	}
-
-	make_vertexShaders();
-	make_fragmentShaders();
-	shaderProgramID = make_shaderProgram();
-
-
-
-
-
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);
@@ -55,16 +41,9 @@ void main(int argc, char** argv) {
 
 
 GLvoid drawScene(GLvoid) {
-	GLclampf base_r = 0.0f;
-	GLclampf base_g = 1.0f;
-	GLclampf base_b = 0.0f;
-
 
 	glClearColor(base_r, base_g, base_b, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glUseProgram(shaderProgramID);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glutSwapBuffers();
 }
@@ -81,65 +60,4 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	}
 
 	glutPostRedisplay();
-}
-
-void make_vertexShaders() {
-	GLchar* vertexSource;
-
-	vertexSource = filetobuf("vertex.glsl");
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
-
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-	if (not result) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: vertex shader error" << errorLog << std::endl;
-		return;
-	}
-}
-void make_fragmentShaders() {
-	GLchar* fragmentSource;
-	fragmentSource = filetobuf("fragment.glsl");
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-	glCompileShader(fragmentShader);
-
-	GLint result;
-	GLchar errorLog[512];
-
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: fragment shader error\n" << errorLog << std::endl;
-		return;
-	}
-}
-GLuint make_shaderProgram() {
-	GLuint shaderID = glCreateProgram();
-	glAttachShader(shaderID, vertexShader);
-	glAttachShader(shaderID, fragmentShader);
-
-	glLinkProgram(shaderID);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	GLint result;
-	GLchar errorLog[512];
-
-
-	glGetProgramiv(shaderID, GL_LINK_STATUS, &result); // ---세이더가 잘 연결되었는지 체크하기
-	if (!result) {
-		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
-		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
-		return false;
-	}
-
-	
-	
-	
-	return shaderID;
 }
