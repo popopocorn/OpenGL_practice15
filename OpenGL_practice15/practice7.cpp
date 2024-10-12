c4bf0806d3e6a510bbd52e29f10f4f57c8b9fb70
popopocorn <popcorn375@naver.com>
2024-10-12 11:10:01 +0900

    쉐이더 틀 작성

--- OpenGL_practice15/practice7.cpp
+++ OpenGL_practice15/practice7.cpp
@@ -2,35 +2,42 @@
 #include<gl/glew.h>
 #include<gl/freeglut.h>
 #include<gl/freeglut_ext.h>
+#include"file_open.h"
 
+#define vertex_shader_code "07_vertex_shader.glsl"
+#define fragment_shader_code "07_fragment_shader.glsl"
 
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 
+void make_vertex_shader();
+void make_fragment_shader();
+GLuint make_shader();
 
-GLclampf base_r = 1.0f;
-GLclampf base_g = 1.0f;
+GLclampf base_r = 0.0f;
+GLclampf base_g = 0.0f;
 GLclampf base_b = 1.0f;
-
+GLint width{ 500 }, height{ 500 };
+GLuint shader_program;
+GLuint vertexShader;
+GLuint fragmentShader;
 
 void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(width, height);
 	glutCreateWindow("Example1");
 
 	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK) {
-		std::cerr << "Unable to initialize GLEW" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		std::cout << "GLEW initialized\n";
-	}
+    glewInit();
+
+    make_vertex_shader();
+    make_fragment_shader();
+    shader_program = make_shader();
+
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);
@@ -44,6 +51,14 @@ GLvoid drawScene(GLvoid) {
 
 	glClearColor(base_r, base_g, base_b, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program);
+
+    glValidateProgram(shader_program);
+
+    glPointSize(5.0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
 	glutSwapBuffers();
 }
@@ -60,4 +75,77 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	}
 
 	glutPostRedisplay();
+}
+
+void make_vertex_shader() {
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    GLchar* vertexSource;
+    vertexSource = file_to_buf(vertex_shader_code);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLint result;
+    GLchar errorLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+        std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+        return;
+    }
+    else {
+        std::cout << "컴파일 성공\n";
+    }
+}
+void make_fragment_shader() {
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    GLchar* fragmentSource;
+    fragmentSource = file_to_buf(fragment_shader_code);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLint result;
+    GLchar errorLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+        std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+        return;
+    }
+    else {
+        std::cout << "컴파일 성공\n";
+    }
+}
+GLuint make_shader() {
+    GLuint shader01;
+    shader01 = glCreateProgram();
+    glAttachShader(shader01, vertexShader);
+    glAttachShader(shader01, fragmentShader);
+
+    glLinkProgram(shader01);
+
+
+    GLint result;
+    GLchar errorLog[512];
+    glGetProgramiv(shader01, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader01, 512, NULL, errorLog);
+        std::cerr << "셰이더 생성 실패\n" << errorLog << std::endl;
+
+    }
+    else {
+        std::cout << "\n컴파일 성공\n";
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+    }
+
+
+    glUseProgram(shader01);
+
+    return shader01;
+
 }
--- OpenGL_practice15/vertex.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-void main(){
-	const vec4 vertex[3]=vec4[3]
-
-	gl_Position = vertex[gl_VertexID];
-
-}
