f9def4eef5d767773e3a4a34289ed3c8f6759ff1
popopocorn <popcorn375@naver.com>
2024-10-21 15:06:45 +0900

    14완

--- OpenGL_practice15/practice14.cpp
+++ OpenGL_practice15/practice14.cpp
@@ -21,6 +21,7 @@ GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 GLvoid SpecialKeyboard(int key, int x, int y);
+GLvoid timer(int);
 
 //------------------------------------------------------
 //셰이더 용 선언
@@ -56,6 +57,8 @@ float dy = 0.0f;
 int draw_face1{ -1 };
 int draw_face2{ -1 };
 
+int spin_flag = -1;// 1=+x 2=-x 3=+y 4=-y
+
 const float cube_color[] = {
     1.0f, 0.0f, 0.0f,
     0.0f, 1.0f, 0.0f,
@@ -83,6 +86,10 @@ const float axis[] = {
     1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
 
 };
+
+float axis_dx{};
+float axis_dy{};
+
 
 //------------------------------------------------------
 //필요한 함수 선언
@@ -126,6 +133,7 @@ void main(int argc, char** argv) {
     glutReshapeFunc(Reshape);
     glutKeyboardFunc(Keyboard);
     glutSpecialFunc(SpecialKeyboard);
+    glutTimerFunc(60, timer, 0);
     read_obj_file("cube.obj", &_cube_13);
     read_obj_file("pyramid.obj", &_diamond_13);
     glEnable(GL_DEPTH_TEST);  // 깊이 테스트 활성화
@@ -148,13 +156,14 @@ GLvoid drawScene(GLvoid) {
     glUseProgram(shader_program);
 
     glValidateProgram(shader_program);
-    
-    glBindVertexArray(a_axis);
-    GLuint modelLoc = glGetUniformLocation(shader_program, "rotate");
     GLuint modelLoc2 = glGetUniformLocation(shader_program, "trans");
+
+
+    glBindVertexArray(a_axis);
+
     glm::mat4 temp(1.0f);
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(temp));
     glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(temp));
+
     glDrawArrays(GL_LINES, 0, 12);
 
     if (is_cull) {
@@ -163,17 +172,13 @@ GLvoid drawScene(GLvoid) {
     else {
         glDisable(GL_CULL_FACE);
     }
+    glm::mat4 tr_mat = glm::mat4(1.0f);
+    tr_mat = glm::translate(tr_mat, glm::vec3(dx, dy, 0.0f));
+    tr_mat = glm::rotate(tr_mat, glm::radians(30.0f + axis_dx), glm::vec3(1.0, 0.0, 0.0));
+    tr_mat = glm::rotate(tr_mat, glm::radians(30.0f + axis_dy), glm::vec3(0.0, 1.0, 0.0));
+    GLuint modloc = glGetUniformLocation(shader_program, "trans");
+    glUniformMatrix4fv(modloc, 1, GL_FALSE, glm::value_ptr(tr_mat));
 
-    glm::mat4 rotate_mat1 = glm::rotate(glm::mat4(1.0f), glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 rotate_mat2 = glm::rotate(glm::mat4(1.0f), glm::radians(-30.0f), glm::vec3(.0f, 0.0f, 1.0f));
-    glm::mat4 rotate_mat = rotate_mat1 * rotate_mat2;
-    modelLoc = glGetUniformLocation(shader_program, "rotate");
-    modelLoc2 = glGetUniformLocation(shader_program, "trans");
-    translate_mat = glm::translate(glm::mat4(1.0f), glm::vec3(dx, dy, 0.0f));
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(rotate_mat));
-    glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(translate_mat));
-
-    
     glBindVertexArray(VAO[0]);
     if(draw_cube){
         if(is_wired){
@@ -186,14 +191,7 @@ GLvoid drawScene(GLvoid) {
             glDrawElements(GL_TRIANGLES, _cube_13.face_count*3, GL_UNSIGNED_INT, 0);
         }
     }
-
- 
-    modelLoc = glGetUniformLocation(shader_program, "rotate");
-
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(rotate_mat));
-    translate_mat = glm::translate(translate_mat, glm::vec3(dx, dy, 0.0f));
-    glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(translate_mat));
-
+    glUniformMatrix4fv(modloc, 1, GL_FALSE, glm::value_ptr(tr_mat));
     glBindVertexArray(VAO[1]);
     if(draw_pyramid){
         if(is_wired){
@@ -228,6 +226,28 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 
     case 'W':
         is_wired = false;
+        break;
+
+    case 'x':
+        spin_flag = 1;
+        break;
+
+    case 'X':
+        spin_flag = 2;
+        break;
+
+    case 'y':
+        spin_flag = 3;
+        break;
+
+    case 'Y':
+        spin_flag = 4;
+        break;
+
+    case 's':
+        spin_flag = -1;
+        dx = 0;
+        dy = 0;
         break;
 
     case 'h':
@@ -386,28 +406,54 @@ GLvoid SpecialKeyboard(int key, int x, int y) {
     switch (key) {
     case GLUT_KEY_UP:
         dy += 0.1f;
-        std::cout << key << std::endl;
-        std::cout << dx << ", " << dy << std::endl;
+
         break;
 
     case GLUT_KEY_DOWN:
         dy -= 0.1f;
-        std::cout << key << std::endl;
-        std::cout << dx << ", " << dy << std::endl;
+
         break;
     case GLUT_KEY_LEFT:
         dx -= 0.1f;
-        std::cout << key << std::endl;
-        std::cout << dx << ", " << dy << std::endl;
+
         break;
     case GLUT_KEY_RIGHT:
         dx += 0.1f;
-        std::cout << key << std::endl;
-        std::cout << dx << ", " << dy << std::endl;
 
     default:
 
         break;
     }
     glutPostRedisplay();
+}
+GLvoid timer(int value) {
+
+    switch (spin_flag)
+    {
+    case 1:
+        axis_dx += 1.0;
+
+        break;
+
+    case 2:
+        axis_dx -= 1.0;
+        break;
+
+    case 3:
+        axis_dy += 1.0;
+        break;
+
+    case 4:
+        axis_dy -= 1.0;
+        break;
+
+    default:
+
+        break;
+    }
+
+    
+
+    glutPostRedisplay();
+    glutTimerFunc(60, timer, 0);
 }
