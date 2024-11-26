9b38a674da76494026bdf055a8a4bc681e61286d
popopocorn <popcorn375@naver.com>
2024-11-26 21:12:35 +0900

    1

--- OpenGL_practice15/floor.h
+++ OpenGL_practice15/floor.h
@@ -1,10 +1,5 @@
 #pragma once
-#include<gl/glew.h>
-#include<gl/freeglut.h>
-#include<gl/freeglut_ext.h>
-#include<gl/glm/ext.hpp>
-#include<gl/glm/glm.hpp>
-#include<gl/glm/gtc/matrix_transform.hpp>
+
 #include<string>
 
 
